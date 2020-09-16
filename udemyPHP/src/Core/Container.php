d107689a2999ba177c6e56a2e083ec596f9e76e0
Christian Pape <60462857+chripa89@users.noreply.github.com>
2020-09-16 02:32:59 +0200

    Update Container.php

--- udemyPHP/src/Core/Container.php
+++ udemyPHP/src/Core/Container.php
@@ -44,8 +44,7 @@ class Container
                 try {
                     $pdo = new PDO(
                         'mysql:host=localhost; dbname=blog',
-                        'root',
-                        ''
+                       
                     );
 
                     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
